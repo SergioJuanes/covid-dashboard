6f324aae82a8df4b39086df51ebb05b7cc9494e5
SergioJuanes <juanessergio@gmail.com>
2022-02-16 17:10:59 +0100

    Defined the skeleton of the app

--- app.R
+++ app.R
@@ -1,59 +1,50 @@
 library(shiny)
 
-# Define UI for app that draws a histogram ----
-ui <- fluidPage(
+#CSS style defined
+css <- '.nav-tabs>li>a {
+  font-family: "Lucida Sans", sans-serif;
+  color: green;
+}'
+
+#Every page separetly
+plot_page <- tabPanel("Plot", 
+                      h5("TextPlot")
+                      )
+
+summary_page <- tabPanel("Summary",
+                         h5("TextSummary")
+                         )
+
+table_page <- tabPanel("Table",
+                       h5("TextTable")
+                       )
+
+# Define UI for random distribution app ----
+ui <- shinyUI(
+  fluidPage(
+  tags$head(tags$style(HTML(css))),
   
   # App title ----
-  titlePanel("Hello Shiny!"),
-  
-  # Sidebar layout with input and output definitions ----
-  sidebarLayout(
-    
-    # Sidebar panel for inputs ----
-    sidebarPanel(
-      
-      # Input: Slider for the number of bins ----
-      sliderInput(inputId = "bins",
-                  label = "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
-      
-    ),
+  titlePanel("Tabsets"),
     
     # Main panel for displaying outputs ----
     mainPanel(
       
-      # Output: Histogram ----
-      plotOutput(outputId = "distPlot")
+      # Output: Tabset w/ plot, summary, and table ----
+      tabsetPanel(type = "tabs",
+                  plot_page,
+                  summary_page,
+                  table_page
+      )
       
     )
-  )
+  
+)
 )
 
-
-# Define server logic required to draw a histogram ----
+# Define server logic for random distribution app ----
 server <- function(input, output) {
-  
-  # Histogram of the Old Faithful Geyser Data ----
-  # with requested number of bins
-  # This expression that generates a histogram is wrapped in a call
-  # to renderPlot to indicate that:
-  #
-  # 1. It is "reactive" and therefore should be automatically
-  #    re-executed when inputs (input$bins) change
-  # 2. Its output type is a plot
-  output$distPlot <- renderPlot({
-    
-    x    <- faithful$waiting
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    hist(x, breaks = bins, col = "#75AADB", border = "white",
-         xlab = "Waiting time to next eruption (in mins)",
-         main = "Histogram of waiting times")
-    
-  })
-  
+
 }
 
 shinyApp(ui, server)
